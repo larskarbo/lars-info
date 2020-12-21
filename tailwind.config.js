0714ce7eea40fb090cebf694339bf4659400a3cd
Lars Karbo <mail@larskarbo.no>
2020-12-21 06:04:03 +0100

    fix pruneboy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.tsx', './public/index.html'],
+  purge: ['./src/**/*.{jsx,js,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
